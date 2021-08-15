f98c53ec5dbbb797ef4c3093fc1ba508d0336bea
kervis vasquez <kervisvasquez24@gmail.com>
2021-08-15 17:56:05 -0400

    validamos que no exista author en la creacion de libros

--- app/Http/Controllers/Book/BookController.php
+++ app/Http/Controllers/Book/BookController.php
@@ -5,21 +5,24 @@ namespace App\Http\Controllers\Book;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use App\Service\BooksService;
+use App\Service\AuthorService;
 use App\Http\Controllers\Controller;
 
 class BookController extends Controller
 {
     use ApiResponse;
     public $bookService;
+    public $authorService;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct( BooksService $booksService)
+    public function __construct( BooksService $booksService, AuthorService $authorService)
     {
         
         $this->booksService = $booksService;
+        $this->authorService = $authorService;
     }
 
     public function index()
@@ -34,6 +37,8 @@ class BookController extends Controller
     }
     public function store(Request $request)
     {
+        /* comprobar si existe un author para el libro */
+        $this->successResponse($this->authorService->getAuthor($request->author_id));
        return $this->successResponse($this->booksService->createBook($request->all()));
     }
     public function update(Request $request, $book_id)
